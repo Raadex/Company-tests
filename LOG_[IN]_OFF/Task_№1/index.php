0b5814556be4546413718743e637476621472550
Radex <korenek01@rambler.ru>
2021-05-05 16:36:20 +0300

    update

--- LOG_[IN]_OFF/Task_№1/index.php
+++ LOG_[IN]_OFF/Task_№1/index.php
@@ -1,23 +1,47 @@
+<?php
+
+if (isset($_POST) && is_numeric($_POST["number1"]) && is_numeric($_POST["number2"])) {
+    $num1 = $_POST["number1"];
+    $num2 = $_POST["number2"];
+    $operand = $_POST["operations"];
+    $result = "";
+    switch ($operand) {
+        case '+':
+            $result = $num1 + $num2;
+            break;
+        case '-':
+            $result = $num1 - $num2;
+            break;
+        case '*':
+            $result = $num1 * $num2;
+            break;
+        case '/':
+            $result = $num2 == 0 ? "на 0 делить нельзя" : $num1 / $num2;
+            break;
+    }
+}
+?>
+
 <!DOCTYPE HTML>
 <html>
- <head>
-  <meta charset="utf-8">
-  <title>Тег SELECT</title>
- </head>
- <body>
-    <form action="controller.php" method="POST">
-        <input type="number" name="number1">
-        <select name="operations" id="">
-            <option value="1">+</option>
-            <option value="2">-</option>
-            <option value="3">*</option>
-            <option value="4">/</option>
-        </select>
-        <input type="number" name="number2">
-        <input type="submit" value="=">
-        <?php if (!empty($_GET['answer'])){ ?>
-            <p>Ответ: <?= $_GET['answer'] ?></p>
-        <?php }?>
-    </form>
- </body>
+<head>
+    <meta charset="utf-8">
+    <title>Тег SELECT</title>
+</head>
+<body>
+<form action="" method="POST">
+    <input type="number" name="number1" step="0.001">
+    <select name="operations" id="">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select>
+    <input type="number" name="number2" step="0.001">
+    <input type="submit" value="=">
+    <?php if (isset($result)) : ?>
+    <p>Ответ: <?= "{$num1} {$operand} {$num2} = {$result}" ?></p>
+    <?php endif ?>
+</form>
+</body>
 </html>
--- LOG_[IN]_OFF/Task_№2/image.php
+++ LOG_[IN]_OFF/Task_№2/image.php
@@ -1,9 +1,17 @@
 <?php
+
 if (!empty($_COOKIE['count_view'])) {
-    setcookie('count_view',$_COOKIE['count_view'] + 1, time() + 86400);
+    setcookie('count_view', $_COOKIE['count_view'] + 1, time() + 86400);
 } else {
     setcookie('count_view', 1, time() + 86400 );
 }
+
 $img = 'image.png';
-header('content-type: image/png');
-readfile($img);
+
+if (file_exists($img)) {
+    header('Content-Type: image/png');
+    header('Content-Length: ' . filesize($img));
+    readfile($img);
+} else {
+    setcookie('count_view', null, -1);
+}
