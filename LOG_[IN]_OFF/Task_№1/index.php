0dfc0e034c7cb1b5291d99887854395f5c126d52
Radex <korenek01@rambler.ru>
2021-05-05 16:39:39 +0300

    second update

--- LOG_[IN]_OFF/Task_№1/index.php
+++ LOG_[IN]_OFF/Task_№1/index.php
@@ -1,47 +1,13 @@
-<?php
-
-if (isset($_POST) && is_numeric($_POST["number1"]) && is_numeric($_POST["number2"])) {
-    $num1 = $_POST["number1"];
-    $num2 = $_POST["number2"];
-    $operand = $_POST["operations"];
-    $result = "";
-    switch ($operand) {
-        case '+':
-            $result = $num1 + $num2;
-            break;
-        case '-':
-            $result = $num1 - $num2;
-            break;
-        case '*':
-            $result = $num1 * $num2;
-            break;
-        case '/':
-            $result = $num2 == 0 ? "на 0 делить нельзя" : $num1 / $num2;
-            break;
-    }
-}
-?>
-
 <!DOCTYPE HTML>
 <html>
-<head>
-    <meta charset="utf-8">
-    <title>Тег SELECT</title>
-</head>
-<body>
-<form action="" method="POST">
-    <input type="number" name="number1" step="0.001">
-    <select name="operations" id="">
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
-    </select>
-    <input type="number" name="number2" step="0.001">
-    <input type="submit" value="=">
-    <?php if (isset($result)) : ?>
-    <p>Ответ: <?= "{$num1} {$operand} {$num2} = {$result}" ?></p>
-    <?php endif ?>
-</form>
-</body>
+ <head>
+  <meta charset="utf-8">
+  <title>Тег SELECT</title>
+ </head>
+ <body>
+ <img src="image.php"/>
+ <?php if (!empty($_COOKIE['count_view'])): ?>
+    <p>Просмотров: <?= $_COOKIE['count_view'] ?></p>
+ <?php endif; ?>
+ </body>
 </html>
